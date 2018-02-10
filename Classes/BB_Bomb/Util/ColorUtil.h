d9139f39487a9bde5d3fcb47d2df3d5737e3762a
caiyin <513274054@qq.com>
2018-02-10 11:17:19 +0800

    删除不需要的文件

--- Classes/BB_Bomb/Util/ColorUtil.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "cocos2d.h"
-
-namespace blutil {
-	void RGB2HSL(const cocos2d::Color3B& color, double &H, double &S, double &L);
-	cocos2d::Color3B HSL2RGB(double H, double S, double L);
-}
--- Classes/Macro.h
+++
@@ -1,3 +0,0 @@
-#define LEVEL_ONE  0.2
-#define LEVEL_TWE  0.4
-#define LEVEL_THREE  27
